af9837c8c1a43189b0c1b428b435c60b6055f241
Vitor Gessner <gessnervgg@gmail.com>
2024-08-17 22:13:30 -0300

    Adicionando alguns códigos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        int numero = 5;
+        System.out.println(++numero);
+
+        int a, b;
+        String resultado = "";
+        a = 6;
+        b = 6;
+
+        resultado = a == b ? "iguais" : "diferentes";
+
+        System.out.println(resultado);
+
+        String nomeUm = "Vitor";
+        String nomeDois = new String("Vitor");
+
+        System.out.println(nomeUm == nomeDois); // false
+        System.out.println(nomeUm.equals(nomeDois)); //true
     }
 }
