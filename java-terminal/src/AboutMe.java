bd77b6175f6c1f791cfea27eda189fc20bcb9455
Vitor Gessner <gessnervgg@gmail.com>
2024-09-02 08:59:39 -0300

    meu projeto conta banco

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println(nome + " " + sobrenome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+}
